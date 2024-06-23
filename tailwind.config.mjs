8c59fcb4dd13713642fdbfb07892740bb7c0434e
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-06-23 16:52:12 +0200

    Feat: Timeline component and Hero section done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        pulseShadow: "pulseShadow 2s infinite",
+      },
+      keyframes: {
+        pulseShadow: {
+          "0%, 100%": {
+            boxShadow: "0 0 0 0 rgba(0, 0, 0, 0.1)",
+          },
+          "50%": {
+            boxShadow: "0 0 15px 10px rgba(0, 0, 0, 0.1)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
